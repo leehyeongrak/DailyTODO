b7624594e85ef8aff27511956409f46cbba3736b
RAK <>
2019-02-20 22:07:17 +0900

    WriteViewController 아울렛변수 및 액션 생성

--- TODOLIST/ViewControllers/WriteViewController.swift
+++ TODOLIST/ViewControllers/WriteViewController.swift
@@ -7,9 +7,66 @@
 //
 
 import UIKit
+import MapKit
 
-class WriteViewController: UIViewController {
-
+class WriteViewController: UIViewController, UITextFieldDelegate {
+    
+    var isToday: Bool = true {
+        didSet {
+            if isToday {
+                todayButton.setTitle("■", for: .normal)
+                tomorrowButton.setTitle("□", for: .normal)
+            } else {
+                todayButton.setTitle("□", for: .normal)
+                tomorrowButton.setTitle("■", for: .normal)
+            }
+        }
+    }
+    
+    @IBOutlet weak var todayButton: UIButton!
+    @IBOutlet weak var tomorrowButton: UIButton!
+    
+    @IBAction func tappedTodayButton(_ sender: UIButton) {
+        isToday = true
+    }
+    @IBAction func tappedTomorrowButton(_ sender: UIButton) {
+        isToday = false
+    }
+    
+    @IBOutlet weak var todoTextField: UITextField!
+    
+    @IBOutlet weak var memoTextField: UITextField!
+    
+    // Time Alarm Setting ---------------------------------------
+    @IBOutlet weak var timeSettingButton: UIButton!
+    
+    @IBAction func tappedTimeSettingButton(_ sender: UIButton) {
+//        timeSettingButton.setTitle("설정■", for: .normal)
+        timeSettingButton.setTitle("설정■", for: .selected)
+        timeSettingButton.isSelected = !timeSettingButton.isSelected
+        
+    }
+    
+    @IBOutlet weak var timePicker: UIDatePicker!
+    // ----------------------------------------------------------
+    
+    // Location Alarm Setting -----------------------------------
+    @IBOutlet weak var locationSettingButton: UIButton!
+    
+    @IBAction func tappedLocationSettingButton(_ sender: UIButton) {
+//        locationSettingButton.setTitle("설정■", for: .normal)
+        locationSettingButton.setTitle("설정■", for: .selected)
+        locationSettingButton.isSelected = !locationSettingButton.isSelected
+    }
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var locationMapView: MKMapView!
+    // ----------------------------------------------------------
+    
+    @IBAction func tappedWriteButton(_ sender: UIButton) {
+    }
+    
     @IBAction func tappedCloseButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -17,17 +74,18 @@ class WriteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        todoTextField.delegate = self
+        memoTextField.delegate = self
         // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
     }
-    */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
