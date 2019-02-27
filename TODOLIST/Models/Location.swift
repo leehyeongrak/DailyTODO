10d757541bda4412bda2f943c8a39443cdfde05d
leehyeongrak <leehrak@gmail.com>
2019-02-27 22:52:13 +0900

    백그라운드 위치트랙킹 및 LocationNotification 구현

--- TODOLIST/Models/Location.swift
+++ TODOLIST/Models/Location.swift
@@ -13,11 +13,21 @@ struct Location {
     var roadAddressName: String
     var x: String
     var y: String
+    var dictionary: [String: Any] {
+        return ["placeName": placeName,
+        "roadAddressName": roadAddressName,
+        "x": x,
+        "y": y]
+    }
     
     init(place: String, address: String, x: String, y: String) {
         self.placeName = place
         self.roadAddressName = address
         self.x = x
         self.y = y
+    }
+    
+    var nsDictionary: NSDictionary {
+        return dictionary as NSDictionary
     }
 }
